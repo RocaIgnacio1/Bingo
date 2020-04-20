bf7e290205f083872b0122b95df8a735cfb271cc
Nacho <nacho@example.com>
2020-04-20 16:08:57 -0300

    Nueva funcion: Al menos una celda ocupada por cada columna

--- test/test_celdas.py
+++ test/test_celdas.py
@@ -1,4 +1,5 @@
 from src.bingo import carton
+from src.bingo import columnas
 
 def test_contar_celdas_ocupadas():
     mi_carton = carton()
@@ -11,9 +12,11 @@ def test_contar_celdas_ocupadas():
 
 def test_al_menos_una_celda_ocupada_por_columna():
     mi_carton = carton()
+    contador_columnas = 0
     contador = 0
-    for filas in mi_carton:
-        for celda in filas:
-            contador = contador + celda
+    for contador_columnas in range(9):
+        mi_columna = columnas(mi_carton,contador_columnas)
+        if sum(mi_columna) >= 1:
+            contador = contador + 1
 
-        assert contador >= 6
+    assert contador == 9
