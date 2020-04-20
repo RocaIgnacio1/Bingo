766d503f43bdfe4e289143b045376318b6a1223d
Nacho <nacho@example.com>
2020-04-20 16:14:23 -0300

    Nueva funcion columnas. Dado un carton y un numero de columna, devuelve la columna

--- src/bingo.py
+++ src/bingo.py
@@ -1,8 +1,13 @@
 
 def carton():
     carton = (
-        (1,0,0,1,1,0,1,0,1),
-        (0,1,1,1,0,1,1,1,1),
-        (0,1,0,0,1,0,0,1,0)
+        (0,1,0,1,1,0,1,0,1),
+        (1,0,1,1,0,1,0,1,1),
+        (0,1,0,0,1,0,0,1,1)
     )
     return carton
+
+def columnas(carton, numero_de_columna):
+    columna = (carton[0][numero_de_columna], carton[1][numero_de_columna], carton[2][numero_de_columna])
+
+    return columna
